d5cae1a7adbeca653a16512b2c350af8cc190677
EC2 Default User <ec2-user@ip-172-31-12-157.ap-southeast-2.compute.internal>
2023-11-05 14:54:37 +0000

    メッセージ

---
+++ memo.rb
@@ -0,0 +1,30 @@
+require "csv" # CSVファイルを扱うためのライブラリを読み込んでいます
+ 
+ puts "1 → 新規でメモを作成する / 2 → 既存のメモを編集する"
+ 
+ memo_type = gets.to_i # ユーザーの入力値を取得し、数字へ変換しています
+ 
+ if memo_type == 1
+   puts"新規メモの内容を入力してください"
+   memo_content = readlines.join("\n").chomp
+   
+   CSV.open("memo.csv","w") do |csv|
+     csv << [memo_content]
+   end
+   
+   puts"メモを作成しました"
+   
+elsif memo_type == 2
+   puts"追記するメモの内容を入力してください"
+   memo_content = readlines.join("\n").chomp
+   CSV.open("memo.csv","a") do |csv|
+     csv << [memo_content]
+   end
+   
+   puts"メモを編集しました"
+   
+ else
+   puts"１か２を入力してください"
+ end
+   
+   
